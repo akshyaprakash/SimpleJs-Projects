beb95a995538e30351362fbca9ded473d401ca64
Akshaya Prakash <akshayaprakash@192-168-1-185.tpgi.com.au>
2021-01-21 13:02:59 +1100

    form validation with if-else

---
+++ Form-Validator/script.js
@@ -0,0 +1,60 @@
+//grab everything from the dom
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+//show input error message
+function showError(input, message){
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+}
+
+//show success message
+
+function showSuccess(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
+//check email is valid
+function isValidEmail(email){
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+//add event listener when we submit it
+form.addEventListener('submit',function(e){
+    //event parameter that we passed in that e value has a method called prevent default
+    //which prevents it from submitting.
+    e.preventDefault();
+    //console.log(username.value);
+    if(username.value === '') {
+        showError(username, 'Username is required')
+        //alert('Username is required');
+    } else {
+        showSuccess(username);
+    }
+    if(email.value === '') {
+        showError(email, 'Email is required')
+        //alert('email is required');
+    } else if (!isValidEmail(email.value)){
+        showError(email, 'Email is not valid')
+    } else {
+        showSuccess(email);
+    }
+    if(password.value === '') {
+        showError(password, 'Password is required')
+        //alert('password is required');
+    } else {
+        showSuccess(password);
+    }
+    if(password2.value === '') {
+        showError(password2, 'Password2 is required')
+        //alert('password2 is required');
+    } else {
+        showSuccess(password2);
+    }
+
+});
+
